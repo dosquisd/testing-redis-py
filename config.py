961b1c09ddeeded793fdb077af321d12285a1042
dosquisd <dosquisd123@gmail.com>
2024-12-06 13:25:55 -0500

    Add enviroment variables

---
+++ config.py
@@ -0,0 +1,51 @@
+from pydantic_core import MultiHostUrl
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from pydantic import PostgresDsn, RedisDsn, computed_field
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
+
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+
+    @computed_field
+    @property
+    def POSTGRES_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            path=self.POSTGRES_DB
+        )
+
+
+    REDIS_USER: str
+    REDIS_PASSWORD: str
+    REDIS_PORT: int
+    REDIS_SERVER: str
+
+
+    @computed_field
+    @property
+    def REDIS_URI(self) -> RedisDsn:
+        return MultiHostUrl.build(
+            scheme="redis",
+            host=self.REDIS_SERVER,
+            port=self.REDIS_PORT,
+            username=self.REDIS_USER,
+            password=self.REDIS_PASSWORD
+        )
+
+
+settings: Settings = Settings()
