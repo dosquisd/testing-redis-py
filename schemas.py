5241bc54b55b42eca2012ebbac3c62b94c28df45
dosquisd <dosquisd123@gmail.com>
2024-12-06 13:27:11 -0500

    Create some schemas for API

---
+++ schemas.py
@@ -0,0 +1,47 @@
+from pydantic import BaseModel
+from typing import TypedDict, Optional
+
+
+class ApiResponseDefault(TypedDict):
+    detail: str
+
+
+class CreateBook(BaseModel):
+    title: str
+    genre: str
+    id_author: int
+
+
+class CreateAuthor(BaseModel):
+    name: str
+    nacionality: str
+
+
+class Authors(BaseModel):
+    id: int
+    name: str
+    nacionality: str
+
+    class Config:
+        from_attributes = True
+
+
+class Books(BaseModel):
+    id: int
+    title: str
+    genre: str
+    id_author: int
+
+    class Config:
+        from_attributes = True
+
+
+class UpdateBook(BaseModel):
+    title: Optional[str]
+    genre: Optional[str]
+    id_author: Optional[int]
+
+
+class UpdateAuthor(BaseModel):
+    name: Optional[str]
+    nacionality: Optional[str]
